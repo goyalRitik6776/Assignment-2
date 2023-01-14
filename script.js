2bfb3136955e2f8464d3c9ea997ecd2f287fc882
goyalritik <ritikg6776@gmail.com>
2023-01-14 23:04:08 +0530

    Second Commit

--- script.js
+++ script.js
@@ -20,29 +20,6 @@ var brickHeight_top = 20;
 
 var brickVisible_top = true;
 
-var ballCollision = false;
-// var bricks = [
-//     {x: canvas.width / 4 - 75 / 2, y: 50, width: 75, height: 20, color: "green", visible: true},
-//     {x: canvas.width / 4 * 3 - 75 / 2, y: 75, width: 75, height: 20, color: "yellow", visible: true},
-//     {x: canvas.width / 4 - 75 / 2, y: 100, width: 75, height: 20, color: "orange", visible: true},
-//     {x: canvas.width / 4 * 3 - 75 / 2, y: 125, width: 75, height: 20, color: "purple", visible: true}
-// ];
-var bricksPerRow = 8;
-    var brickPadding = 10;
-    var brickOffsetTop = 50;
-    var brickOffsetLeft = 30;
-    var brickWidthTop = (canvas.width - (brickOffsetLeft * 2) - (brickPadding * (bricksPerRow - 1))) / bricksPerRow;
-    var brickHeightTop = 20;
-    var bricks = [];
-
-    for (var c = 0; c < bricksPerRow; c++) {
-        for (var r = 0; r < 3; r++) {
-            var brickXTop = (c * (brickWidthTop + brickPadding)) + brickOffsetLeft;
-            var brickYTop = (r * (brickHeightTop + brickPadding)) + brickOffsetTop;
-            bricks.push({ x: brickXTop, y: brickYTop, width: brickWidthTop, height: brickHeightTop, color: "blue", visible: true });
-        }
-    }
-
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
 
@@ -52,7 +29,6 @@ function drawBall() {
     ctx.fillStyle = "white";
     ctx.fill();
     ctx.closePath();
-    // createCanvas(400,400);
 }
 function drawBrick() {
     ctx.beginPath();
@@ -61,21 +37,12 @@ function drawBrick() {
     ctx.fill();
     ctx.closePath();
 }
-// function drawBrickTop() {
-//     ctx.beginPath();
-//     ctx.rect(brickX_top, brickY_top, brickWidth_top, brickHeight_top);
-//     ctx.fillStyle = "red";
-//     ctx.fill();
-//     ctx.closePath();
-// }
 function drawBrickTop() {
-    for (var i = 0; i < bricks.length; i++) {
-        ctx.beginPath();
-        ctx.rect(bricks[i].x, bricks[i].y, bricks[i].width, bricks[i].height);
-        ctx.fillStyle = bricks[i].color;
-        ctx.fill();
-        ctx.closePath();
-    }
+    ctx.beginPath();
+    ctx.rect(brickX_top, brickY_top, brickWidth_top, brickHeight_top);
+    ctx.fillStyle = "red";
+    ctx.fill();
+    ctx.closePath();
 }
 function keyDownHandler(e) {
     if (e.key == "Right" || e.key == "ArrowRight") {
@@ -95,7 +62,6 @@ function keyUpHandler(e) {
     }
 }
 function detectCollision() {
-    ballCollision = false;
     if (ballX + ballRadius > brickX && ballX - ballRadius < brickX + brickWidth &&
         ballY + ballRadius > brickY && ballY - ballRadius < brickY + brickHeight) {
         ballSpeedY = -ballSpeedY;
@@ -108,36 +74,11 @@ function detectCollision() {
         ballY + ballRadius > brickY && ballY - ballRadius < brickY + brickHeight) {
         ballSpeedX = -ballSpeedX;
     }
-    // if(brickVisible_top){
-    //     if (ballX + ballRadius > brickX_top && ballX - ballRadius < brickX_top + brickWidth_top &&
-    //         ballY + ballRadius > brickY_top && ballY - ballRadius < brickY_top + brickHeight_top) {
-    //         ballSpeedY = -ballSpeedY;
-    //         brickVisible_top = false;
-    //     }
-
-    // }
-
-    for (var i = 0; i < bricks.length; i++) {
-        if (ballX + ballRadius > bricks[i].x && ballX - ballRadius < bricks[i].x + bricks[i].width &&
-            ballY + ballRadius > bricks[i].y && ballY - ballRadius < bricks[i].y + bricks[i].height) {
+    if(brickVisible_top){
+        if (ballX + ballRadius > brickX_top && ballX - ballRadius < brickX_top + brickWidth_top &&
+            ballY + ballRadius > brickY_top && ballY - ballRadius < brickY_top + brickHeight_top) {
             ballSpeedY = -ballSpeedY;
-            bricks[i].visible = false;
-            bricks[i].x = -1000;
-            bricks[i].y = -1000;
-        }
-        if (ballX + ballRadius > bricks[i].x && ballX - ballRadius < bricks[i].x &&
-            ballY + ballRadius > bricks[i].y && ballY - ballRadius < bricks[i].y + bricks[i].height) {
-            ballSpeedX = -ballSpeedX;
-            bricks[i].visible = false;
-            bricks[i].x = -1000;
-            bricks[i].y = -1000;
-        }
-        if (ballX + ballRadius > bricks[i].x + bricks[i].width && ballX - ballRadius < bricks[i].x + bricks[i].width &&
-            ballY + ballRadius > bricks[i].y && ballY - ballRadius < bricks[i].y + bricks[i].height) {
-            ballSpeedX = -ballSpeedX;
-            bricks[i].visible = false;
-            bricks[i].x = -1000;
-            bricks[i].y = -1000;
+            brickVisible_top = false;
         }
     }
 
@@ -148,12 +89,12 @@ function update() {
     drawBall();
     drawBrick();
     detectCollision();
-        drawBrickTop();
+    drawBrickTop();
     
-    // if(brickVisible_top){
-    //     // brickX_top = -1000;
-    //     // brickY_top = -1000;
-    // }
+    if(!brickVisible_top){
+        brickX_top = -1000;
+        brickY_top = -1000;
+    }
         
     ballX += ballSpeedX;
     ballY += ballSpeedY;
@@ -161,24 +102,14 @@ function update() {
     if (ballX + ballRadius > canvas.width || ballX - ballRadius < 0) {
         ballSpeedX = -ballSpeedX;
     }
-
-    // if (ballY + ballRadius > canvas.height || ballY - ballRadius < 0) {
-    //     ballSpeedY = -ballSpeedY;
-    // }
     if (ballY + ballRadius > canvas.height) {
         ballSpeedY = -ballSpeedY;
         ballY = canvas.height - ballRadius;
     }
     if(ballY - ballRadius < 0){
-            ballSpeedY = -ballSpeedY;
+        ballSpeedY = -ballSpeedY;
 
     }
-        // check if ball collides with white brick
-    if (ballY + ballRadius >= brickY && ballX + ballRadius > brickX && ballX - ballRadius < brickX + brickWidth) {
-            ballSpeedY = -ballSpeedY;
-            ballY = brickY - ballRadius;
-    }
-
     if (rightPressed) {
         brickX += 5;
         if (brickX + brickWidth > canvas.width) {
